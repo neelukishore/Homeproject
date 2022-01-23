19dffb5627bea9fbea484b932751e87253263f74
neelukishore <pneelakantham@gmail.com>
2022-01-23 12:13:30 +0530

    dic change

--- python-check/dictinory.py
+++ python-check/dictinory.py
@@ -18,7 +18,21 @@ for item in student.items():
 	print(item)
 
 for key,value in student.items():
-    print(key,value)     	 
+    print(key,value)   
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "variants": ['figo','aspire', 'freestyle', 'ecosport']
+}
+
+for each_key, each_val in car.items():
+    if each_key == "variants":
+        #print(each_key+"<-->"+str(each_val))
+        if "jhkfsfs" not in each_val:
+            print("Required variant not found")  	 
 
 
 
