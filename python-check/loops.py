8e464400003398fa4f04e1a2df3e4f3e1c076a32
neelaveni <pneelaveni90@gmail.com>
2022-04-21 17:10:20 +0530

    loops

---
+++ python-check/loops.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+nums = [1,2,3,4,5]
+
+######using break ######
+# for num in nums:
+#     if num == 3 :
+#         print('found')
+#         break
+#     print(num) 
+
+######using continue ######
+# for num in nums:
+#     if num == 3 :
+#         print('found')
+#         continue
+#     print(num) 
+
+
+# x =0
+
+# while x < 10 :
+#     if x == 5 :
+#         break
+#     print(x)
+#     x += 1
+
+x =0
+while True:
+    print(x)
+    x += 1
+
+    
+
+
