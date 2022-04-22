9918267aa9af82223217ce965bc7979b8ec5316f
neelaveni <pneelaveni90@gmail.com>
2022-04-22 18:21:19 +0530

    function practice

---
+++ python-check/funcprac.py
@@ -0,0 +1,98 @@
+#¡/usr/bin/python
+
+# def myfunction():
+
+#     print("hello world !")
+
+# myfunction()    
+
+# def myfunction():
+    
+#     return "hello world!"
+
+# print(myfunction())    
+# print(type(myfunction()))
+# print(len(myfunction()))
+# print(myfunction().upper())
+# print(myfunction().title())
+
+# def myfunction(fname,lname):
+#     return '{}{} @gmail.com'.format(fname,lname)
+
+# print(myfunction('nandana','riyan') )
+
+
+# def my_function(country = "Norway"):
+#     return 'I am from {}'.format(country)  
+
+# print(my_function("Sweden"))
+# print(my_function("India"))
+# print(my_function())
+# print(my_function("Brazil"))
+
+# def my_function(food):
+#   for x in food:
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+
+# my_function(fruits) 
+
+
+# def myfunction(x):
+#     return 5*x
+
+# print(myfunction(2))
+# print(myfunction(3))
+# print(myfunction(4))    
+
+
+######recursion#####
+
+
+
+
+# def myrecursion(k):
+#     if(k > 0):
+#         result = k + myrecursion(k - 1)
+#         print(result)
+#     else:
+#         result = 0
+#     return result   
+
+# print("\n\n recursion example")
+# myrecursion(5)            
+
+
+######factorial of number#######
+# def factorial(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+
+# print(factorial(7))
+
+
+######## fibonacci sequance########
+
+# def fib(n):
+#     if n==0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     else:
+#         return fib(n-1) +fib(n-2)
+
+# nterms = int(input("enter the terms:"))
+# if nterms <= 0:
+#     print("please enter positive integer")
+# else:
+#     print("fibonacci sequance")
+#     for i in range(nterms):
+#         print(fib(i))    
+
+
+#https://medium.com/co-learning-lounge/recursive-function-python-practice-examples-c37df75555e8          
+
+
