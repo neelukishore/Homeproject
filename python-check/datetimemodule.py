785873e98ffc130f4c36187b062a79d125c09cca
neelaveni <pneelaveni90@gmail.com>
2022-04-27 13:31:53 +0530

    datetime practice

---
+++ python-check/datetimemodule.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import datetime
+
+# d = datetime.date(2016,7,24)
+# print(d)
+
+# td = datetime.date.today()
+# print(td)
+# print(td.day)
+# print(td.weekday())
+# print(td.isoweekday())
+# print(td.isocalendar())
+
+# t = datetime.time(9,30,45,100000)
+# print(t)
+# print(t.microsecond)
+
+# dt = datetime.datetime.today()
+# dt1 = datetime.datetime.now()
+# dt2 = datetime.datetime.utcnow()
+
+# print(dt)
+# print(dt1)
+# print(dt2)
+
