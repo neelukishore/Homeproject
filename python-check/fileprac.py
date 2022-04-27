4f664e2c1650545a0d457cb3050a9cc9b4e21171
neelaveni <pneelaveni90@gmail.com>
2022-04-27 13:32:08 +0530

    file practice

---
+++ python-check/fileprac.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# with open('demofile.txt','r') as f:
+#     print(f.mode)
+#     print(f.read())
+#     print(f.close())    
+
+#f = open('demofile.txt','r')
+# print(f.readline())
+# print(f.readline())
+# print(f.close())
+# for x in f:
+#     print(x)
+
+# f_contents = f.read()
+# print(f_contents)
+
+# f = open("demofile.txt","r")
+# size_value = 10
+# f_contents = f.read(size_value)
+# print(f_contents,end=' ' )
+# f.seek(0)
+# f_contents = f.read(size_value)
+# print(f_contents )
+
+# f = open("demofile.txt","r")
+# fw = open("text.txt","w")
+# for line in f:
+#     fw.write(line)
